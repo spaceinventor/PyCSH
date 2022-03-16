aeb1c549930790c1da3e35269fde70921cc0276e
kivkiv12345 <kevincarlsen@live.dk>
2022-03-16 09:22:31 +0100

    Fix paths for automatic compilation.

--- __init__.py
+++ __init__.py
@@ -1,19 +1,20 @@
 """ Thin wrapper package for the CSH python bindings. Automatically attempts to compile when import fails. """
 
 try:  # Import everything from the bindings themselves.
-    try:  # Using relative import.
+    try:  # Using relative import
         from .builddir.pycsh import *
     except ImportError:  # When attempting to import with no known parent package.
         from builddir.pycsh import *
 except ModuleNotFoundError:  # Let's try and recompile the bindings.
     import os as _os
     import sys as _sys
-    _abspath = _os.path.dirname(__file__)  # Use full paths for safety.
-    print('Failed to import libparam Python bindings attempting to recompile.')
+    _abspath = _os.path.dirname(__file__)
+    _builddir = _os.path.join(_abspath, 'builddir')
+    print('Failed to import CSH Python bindings. Attempting to recompile...')
     # Compile for our current version of Python.
-    _os.system(f"rm -r {_abspath}/builddir; "
-               f"meson {_abspath}/ {_abspath}/builddir -Dpython3_versions=python3.{_sys.version_info.minor} && "
-               f"ninja -C {_abspath}/builddir")
+    _os.system(f"rm -r {_builddir}; "
+               f"meson {_abspath or '.'} {_builddir} -Dpython3_versions=python3.{_sys.version_info.minor} && "
+               f"ninja -C {_builddir}")
     try:  # Using relative import.
         from .builddir.pycsh import *  # Welp, here goes nothing...
     except ImportError:  # When attempting to import with no known parent package.
