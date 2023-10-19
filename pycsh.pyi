912bdc18fcc4db1076eb12327023ae488f14d7ee
william <william@space-inventor.com>
2023-10-19 10:37:39 +0200

    fixed minor bugs in pyi file. And made some minor changes to the optional arguments

--- pycsh.pyi
+++ pycsh.pyi
@@ -471,7 +471,7 @@ def list_save(filename: str = None, node: int = None, include_node: bool = True)
     :param include_node: Whether to include the node of the parameter in the "last add" command.
     :returns: Currently returns None, may return string of "list add" commands in future versions.
     """
-def list_add(node: int, length: int, id: int = None, name: str = None, type: int = None, mask: int = None,  comment: str = None, unit: str = None) -> None:
+def list_add(node: int, length: int, id: int, name: str, type: int, mask: int = None,  comment: str = None, unit: str = None) -> None:
     """
     Add a parameter to the global list
 
