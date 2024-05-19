7cf27fb1904d4e8f02a8970ac0d37043275e7a83
kivkiv12345 <kevincarlsen@live.dk>
2024-05-19 14:53:38 +0200

    Auto release GIL in Parameter_callback()

--- src/parameter/pythonparameter.c
+++ src/parameter/pythonparameter.c
@@ -22,7 +22,7 @@ PyObject * PyExc_InvalidParameterTypeError;
  * @brief Shared callback for all param_t's wrapped by a Parameter instance.
  */
 void Parameter_callback(param_t * param, int offset) {
-    PyGILState_STATE gstate = PyGILState_Ensure();
+    PyGILState_STATE CLEANUP_GIL gstate = PyGILState_Ensure();
     assert(Parameter_wraps_param(param));
     assert(!PyErr_Occurred());  // Callback may raise an exception. But we don't want to override an existing one.
 
@@ -34,7 +34,6 @@ void Parameter_callback(param_t * param, int offset) {
         Perhaps it was deleted? Or perhaps it was never set correctly. */
     if (python_param == NULL) {
         assert(false);  // TODO Kevin: Is this situation worthy of an assert(), or should we just ignore it?
-        PyGILState_Release(gstate);
         return;
     }
 
@@ -44,7 +43,6 @@ void Parameter_callback(param_t * param, int offset) {
     /* This Parameter has no callback */
     /* Python_callback should not be NULL here when Parameter_wraps_param(), but we will allow it for now... */
     if (python_callback == NULL || python_callback == Py_None) {
-        PyGILState_Release(gstate);
         return;
     }
 
@@ -65,7 +63,6 @@ void Parameter_callback(param_t * param, int offset) {
         // TODO Kevin: It seems exceptions raised in the CSP thread are ignored.
         _PyErr_FormatFromCause(PyExc_ParamCallbackError, "Error calling Python callback");
     }
-    PyGILState_Release(gstate);
 }
 
 // Source: https://chat.openai.com
