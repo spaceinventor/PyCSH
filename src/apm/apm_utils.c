45874841a4ed4d90e15d55f79afc5401b51d9a99
william <william@space-inventor.com>
2024-10-22 10:23:49 +0200

    Added newline in error print

--- src/apm/apm_utils.c
+++ src/apm/apm_utils.c
@@ -171,7 +171,7 @@ PyObject * pycsh_load_pymod(const char * const _filepath, const char * const ini
 		PyObject *init_function AUTO_DECREF = PyObject_GetAttrString(pModule, init_function_name);
 		if (init_function == NULL) {
 			PyErr_Clear();
-			printf("Skipping missing init function '%s()' in module '%s'", init_function_name, filename);
+			printf("Skipping missing init function '%s()' in module '%s'\n", init_function_name, filename);
 			//fprintf(stderr, "Cannot find function \"%s()\" in %s\n", init_function_name, filename); // This print is a good idea for debugging, but since the apm_init(main) is not required this print can be a bit confusing.
 			return Py_NewRef(pModule);
 		}
