81377d3878fcf7975db493c58b7266b13a858e31
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2025-06-06 14:09:30 +0200

    Split into pycsh/pycsh_core and use new pycsh_core dependency

--- pycsh_core/src/wrapper/slash_py.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-
-PyObject * pycsh_slash_execute(PyObject * self, PyObject * args, PyObject * kwds);
--- pycsh_core/src/wrapper/victoria_metrics_py.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-
-PyObject * pycsh_vm_start(PyObject * self, PyObject * args, PyObject * kwds);
-PyObject * pycsh_vm_stop(PyObject * self, PyObject * args);
